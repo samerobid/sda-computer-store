9f7eaf674f3b70a6d92486a4f62b159ea04d2b02
raonn <kaankaraoglu@outlook.com>
2017-01-29 18:30:38 +0100

    Implements addition & removal of computers and printing all info regarding peripherals.

--- part-2-store/src/ComputerStore.java
+++ part-2-store/src/ComputerStore.java
@@ -3,8 +3,38 @@ import java.util.ArrayList;
 public class ComputerStore {
 
     private ArrayList<Computer> computers;
-    
-        public ComputerStore(){
+    private int index;
 
+    // Constructor
+    public ComputerStore(){
+        computers = new ArrayList<>();
+    }
+
+    // Adds a Computer into "computers" ArrayList
+    public boolean addComputer(Computer newComputer) {
+        return computers.add(newComputer);
+    }
+
+    // Removes a Computer object from "computers" ArrayList
+    public boolean removeComputer(int index) {
+        this.index = index;
+        computers.remove(index);
+        return true;
+    }
+
+    // Prints out all the details of each computer in store
+    public void printAllComputers( ) {
+        for(Computer computer : computers) {
+            computer.printSummary();
         }
+    }
+
+    // Prints the total cost for all computers
+    public void costOfAllComputers( ) {
+        int total = 0;
+        for(Computer computer : computers) {
+            total = total + computer.individualPrice();
+        }
+        System.out.println("Total price of all computers in store: " + total + "kr");
+    }
 }
