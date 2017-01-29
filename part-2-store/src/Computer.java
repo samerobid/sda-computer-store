abef5313a65d112d90656e940fe8dad5ed7e5de0
raonn <kaankaraoglu@outlook.com>
2017-01-29 14:32:27 +0100

    Copied necessary classes from part-1 and created ComputerStore class.

---
+++ part-2-store/src/Computer.java
@@ -0,0 +1,41 @@
+public class Computer {
+
+    private Processor processor;
+    private HardDrive harddrive;
+    private Display display;
+
+    // Constructor
+    public Computer() {
+        processor = new Processor(6500,2000,"Xeon","Intel");
+        harddrive = new HardDrive(1500,0,256,"Corsair");
+        display = new Display(4000,60,"Asus","3840 x 2160");
+    }
+
+    // Prints the names and cost of peripherals
+    public void printComputerSummary() {
+
+        // Print Processor Information
+        System.out.println("Processor Info:");
+        System.out.println("_______________");
+        System.out.println("Manufacturer: " + processor.getManufacturer());
+        System.out.println("Codename(model): " + processor.getModel());
+        System.out.println("Clock Speed: " + processor.getClockspeed() + " MHz");
+        System.out.println("Price: " + processor.getCost() + "kr" + "\n\n");
+
+        // Print HardDrive Information
+        System.out.println("HardDrive Info:");
+        System.out.println("_______________");
+        System.out.println("Manufacturer: " + harddrive.getManufacturer());
+        System.out.println("RPM: " + harddrive.getRpm() + " (SSD)");
+        System.out.println("Capacity: " + harddrive.getCapacity() + "GB");
+        System.out.println("Price: "  + harddrive.getCost() + " kr" + "\n\n");
+
+        // Print Display Information
+        System.out.println("Display Info:");
+        System.out.println("_____________");
+        System.out.println("Manufacturer: " + display.getManufacturer());
+        System.out.println("Resolution: " + display.getResolution());
+        System.out.println("Refresh Rate: " + display.getRefreshRate() + "Hz");
+        System.out.println("Price: " + display.getCost() + "kr");
+    }
+}
---
+++ part-2-store/src/ComputerStore.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class ComputerStore {
+
+    private ArrayList<Computer> computers;
+    
+        public ComputerStore(){
+
+        }
+}
