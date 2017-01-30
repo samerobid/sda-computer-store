42c518719950643144130c2640e1e8064cda3df6
raonn <kaankaraoglu@outlook.com>
2017-01-30 23:51:40 +0100

    Beautiful outputs and proper instance creation!

--- part-2-store/src/Main.java
+++ part-2-store/src/Main.java
@@ -3,17 +3,22 @@ public class Main {
 
         ComputerStore awesomeStore = new ComputerStore();
 
-        Computer SuperComputer = new Computer();
-        Computer DuperComputer = new Computer();
+        Processor IntelXeon = new Processor(5000, 2600, "Xeon", "Intel");
+        Display AsusLED = new Display(5000, 144, "Asus", "2560x1440");
+        HardDrive CorsairSSD = new HardDrive(1200, 0, 256, "Corsair");
+
+        Processor Corei7 = new Processor(2000, 4000, "Corei7", "Intel");
+        Display DellLED = new Display(3500, 60, "DELL", "3840x2160");
+        HardDrive IntelSSD = new HardDrive(1200, 0, 512, "Intel");
+
+        Computer SuperComputer = new Computer(IntelXeon, CorsairSSD, AsusLED);
+        Computer DuperComputer = new Computer(Corei7, IntelSSD, DellLED);
 
         awesomeStore.addComputer(SuperComputer);
         awesomeStore.addComputer(DuperComputer);
 
-        SuperComputer.printSummary();
-        DuperComputer.printSummary();
-
-        SuperComputer.individualPrice();
-        DuperComputer.individualPrice();
+        System.out.println("Price of this computer is: " + SuperComputer.getPrice() + "kr");
+        System.out.println("Price of this computer is: " + DuperComputer.getPrice() + "kr");
 
         awesomeStore.getTotalCost();
     }
