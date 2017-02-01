3ddb519c1a6a4dad5093c92e16a9b4ec147268b9
samerobid <samerobid@gmail.com>
2017-02-01 19:02:30 +0100

    Create Processor.java

---
+++ part-1-basics/Processor.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class Processor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Processor
+{
+    public String manufacturer;
+    public String speed;
+    public String model;
+    public int cost;
+    
+   
+
+    public Processor()
+    {
+        
+        manufacturer = "Intel";
+        speed = "3600 GH";
+        model = "I7";
+        cost = 4000;
+    }
+    
+    public void tr()
+    {
+        System.out.println("Hi");
+        
+    }
+}
